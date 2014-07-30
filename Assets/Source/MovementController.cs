5af85b0a88dbf34f1b0c0fb5c7d66525d191ca3b
Kitane <skyvlk66@gmail.com>
2014-07-30 15:58:50 +0200

    Fixing the movement (per Milda's change)

--- Assets/Source/MovementController.cs
+++ Assets/Source/MovementController.cs
@@ -29,11 +29,12 @@ public class MovementController : MonoBehaviour {
 		{
 			var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hit;
+			/*
 			if (Physics.Raycast (ray, out hit, 1000))
 			{
 				//WaypointPlanner.AddWaypoint(hit.point);
 				_navMeshAgent.SetDestination(hit.point);
-			}
+			}*/
 
 			GameObject clickedObject = GetClickedGameObject(out hit);
 
