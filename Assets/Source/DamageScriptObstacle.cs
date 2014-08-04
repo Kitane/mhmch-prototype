8c0c32e1f593899e3fd7481eb5bd1ef328a3e188
Malek, Milan <malek.milan@gmail.com>
2014-08-04 13:40:29 +0200

    Obstacles stops fire.

---
+++ Assets/Source/DamageScriptObstacle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageScriptObstacle : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		var projectile = other.gameObject.GetComponentInParent<ProjectileScript>();
+		if (projectile != null)
+		{
+			Debug.Log("obstacle damage:" + projectile.Definition.Damage);
+			projectile.Impact(other.gameObject.transform.position);
+		}
+	}
+}
