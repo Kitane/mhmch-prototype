4c7f3677cb1bed8a5468b39e1c8405297136646e
Malek, Milan <malek.milan@gmail.com>
2014-08-07 11:22:20 +0200

    Add free NGUI for basic UI.

---
+++ Assets/Source/UI/NGUI/AddUnitEnemyHUD.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Example script that instantiates a HUD window that will follow this game object.
+/// </summary>
+
+[AddComponentMenu("NGUI/Examples/Add Unit Enemy HUD")]
+public class AddUnitEnemyHUD : MonoBehaviour
+{
+	public GameObject prefab;
+	public ActorScript actorScript;
+	
+	void Start ()
+	{
+		if (prefab != null)
+		{
+			UICamera cam = UICamera.FindCameraForLayer(prefab.layer);
+			
+			if (cam != null)
+			{
+				GameObject go = cam.gameObject;
+				UIAnchor anchor = go.GetComponent<UIAnchor>();
+				if (anchor != null) go = anchor.gameObject;
+				
+				GameObject child = GameObject.Instantiate(prefab) as GameObject;
+				Transform t = child.transform;
+				t.parent = go.transform;
+				t.localPosition = Vector3.zero;
+				t.localRotation = Quaternion.identity;
+				t.localScale = Vector3.one;
+				
+				UnitEnemyHUD hud = child.GetComponent<UnitEnemyHUD>();
+				if (hud != null)
+				{
+					hud.target = transform;
+					hud.actorScript = actorScript;
+				}
+			}
+			else
+			{
+				Debug.LogWarning("No camera found for layer " + LayerMask.LayerToName(prefab.layer), gameObject);
+			}
+		}
+		Destroy(this);
+	}
+}
---
+++ Assets/Source/UI/NGUI/UnitEnemyHUD.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+//https://www.youtube.com/watch?v=idVr8-jfdAI
+//http://www.tasharen.com/forum/index.php?topic=130.0
+
+/// <summary>
+/// Example script showing how to add UI window that follows an object drawn by another camera.
+/// </summary>
+
+[AddComponentMenu("NGUI/Examples/Unit Enemy HUD")]
+public class UnitEnemyHUD : MonoBehaviour
+{
+	/// <summary>
+	/// Target object this UI element should follow.
+	/// </summary>
+	
+	public Transform target;
+	public UISlider progressBar;
+	public ActorScript actorScript;
+	
+	Transform mTrans;
+	Camera mGameCam;
+	Camera mUICam;
+	Vector3 mPos;
+	bool mVisible = true;
+	
+	void Start ()
+	{
+		if (target == null) { Destroy(gameObject); return; }
+		mTrans = transform;
+		mGameCam = NGUITools.FindCameraForLayer(target.gameObject.layer);
+		mUICam = NGUITools.FindCameraForLayer(gameObject.layer);
+	}
+	
+	void LateUpdate()
+	{
+		if (target == null) { Destroy(gameObject); return; }
+		
+		mPos = mGameCam.WorldToViewportPoint(target.position);
+		
+		bool visible = (mPos.z > 0f && mPos.x > 0f && mPos.x < 1f && mPos.y > 0f && mPos.y < 1f);
+		
+		if (mVisible != visible)
+		{
+			mVisible = visible;
+			UIWidget[] widgets = gameObject.GetComponentsInChildren<UIWidget>();
+			foreach (UIWidget w in widgets) w.enabled = mVisible;
+		}
+		
+		if (mVisible)
+		{
+			mPos = mUICam.ViewportToWorldPoint(mPos);
+			mPos.z = 0f;
+			mTrans.position = mPos;
+
+			UpdateValue();
+		}
+	}
+
+	private void UpdateValue()
+	{
+		if (progressBar != null && actorScript != null)
+		{
+			float healthStatus = actorScript.Health / actorScript.MaxHealth;
+			progressBar.sliderValue = healthStatus;
+		}
+	}
+}
